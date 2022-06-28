4f77df2c202bd31d65509a2373ea8dc574a9c364
DESKTOP-3BRQ0CN\user <aaaa@gmail.com>
2022-06-28 18:45:02 +0900

    jsp + timezone 시간 세팅까지

---
+++ src/main/java/com/example/lesson04/bo/UserBO.java
@@ -0,0 +1,24 @@
+package com.example.lesson04.bo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.lesson04.dao.UserDAO;
+
+@Service
+public class UserBO {
+	
+	@Autowired
+	UserDAO userDAO;
+	
+	
+	public void addUserAsField(String name, String yyyymmdd, String email, String introduce) {
+		
+		userDAO.insertUserAsField(name, yyyymmdd, email, introduce);
+	}
+	
+	public NewUser getLastUser() {
+		return userDAO.selectLastUser();
+	}
+	
+}
